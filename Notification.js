d1c510cabc9db822d1c2cba140bc511aaf30224b
JannekRC <jan.nek@live.dk>
2022-05-06 16:13:15 +0300

    more notif

--- Notification.js
+++ Notification.js
@@ -1,6 +1,7 @@
 import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import storage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -15,27 +16,26 @@ export default function Notification() {
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
   const responseListener = useRef();
-  const getPermission = async () => {
-    const { status: existingStatus } =
-      await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== "granted") {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== "granted") {
-      alert("Without permission, you won't recieve habit reminder notifications!");
-      await storage.setItem("expopushtoken", "");
-      return;
-    }
-    const token = (await Notifications.getExpoPushTokenAsync()).data;
-    await storage.setItem("expopushtoken", token);
-  };
 
   useEffect(() => {
-    registerForPushNotificationsAsync().then((token) =>
-      setExpoPushToken(token)
-    );
+    const getPermission = async () => {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        alert("Without permission, you won't recieve habit reminder notifications!");
+        await storage.setItem("expopushtoken", "");
+        return;
+      }
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      await storage.setItem("expopushtoken", token);
+    }
+    getPermission();
+  }, []);
 
     notificationListener.current =
       Notifications.addNotificationReceivedListener((notification) => {
